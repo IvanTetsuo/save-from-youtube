8ca306ad26b3f8f67b9563faa875a613c3d8dc53
IvanTetsuo <ivan.d2413@gmail.com>
2025-01-27 18:36:18 +0300

    добавлен процесс обработки очереди Bull

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,18 @@ import express from 'express';
 import router from './routes';
 const app = express();
 const port = process.env.PORT || 3000;
+import { videoDownloadingQueue } from './queue';
+import { videoDownloader } from './services/VideoDownloader';
 
 app.use(express.json());
 app.use('/', router);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
+});
+
+videoDownloadingQueue.process(async (job) => {
+    const isDownloaded = await videoDownloader.downloadVideo(job.data.url, job.id.toString());
+    if (!isDownloaded) {
+        throw new Error('Downloading failed');
+    }
 });
