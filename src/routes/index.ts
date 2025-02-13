93cc4d6f48dbeaf088f5619726eae50d5a524a41
IvanTetsuo <ivan.d2413@gmail.com>
2025-02-13 15:39:50 +0300

    добавлен роут download-video

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { routerApi } from './api';
+import { downloadVideo } from './downloadVideo';
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index');
 });
 
 router.use('/api', routerApi);
+router.get('/download-video/:token', downloadVideo);
 
 export default router;
