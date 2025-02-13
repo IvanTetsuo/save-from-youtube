9494d6a5824501a310921d536b65bef8ba2d2c4a
IvanTetsuo <ivan.d2413@gmail.com>
2025-02-13 15:35:50 +0300

    добавлен роут get-download-link

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { addVideoDownloadTask } from './addVideoDownloadTask';
 import { getResult } from './getResult';
+import { getDownloadLink } from './getDownloadLink';
+import path from 'path';
 const router = Router();
 
 router.get('/', (req, res) => {
@@ -8,5 +10,6 @@ router.get('/', (req, res) => {
 });
 router.post('/add-video-download-task', addVideoDownloadTask);
 router.get('/get-video-download-result', getResult);
+router.get('/get-download-link', getDownloadLink);
 
 export {router as routerApi};
