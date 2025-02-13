0f6b4b45ae70cdd8226ca2419978fdc9d860c97d
IvanTetsuo <ivan.d2413@gmail.com>
2025-02-13 15:34:35 +0300

    изменён обработчик получения статуса скачивания

--- src/routes/api/getResult.ts
+++ src/routes/api/getResult.ts
@@ -1,8 +1,25 @@
 import { Request, Response } from "express";
 import { videoDownloader } from "../../services/VideoDownloader";
+import { JobState } from "bullmq";
+
+type VideoDownloadingResult = {
+    result: boolean,
+    status: 'unknown' | JobState,
+    downloadVideoUrl?: string,
+}
 
 export const getResult = async (req: Request, res: Response) => {
-    const token = req.query.token;
-    const status = await videoDownloader.getJobStatus(token as string);
-    return res.json({result: true, status});
+    const token = req.query.token as string;
+    const status = await videoDownloader.getJobStatus(token);
+    if (!status) {
+        return res.status(404).json({error: 'Отсутствует статус у видео'});
+    }
+    const result: VideoDownloadingResult = {
+        result: true,
+        status,
+    }
+    if (status === 'completed') {
+        result.downloadVideoUrl = `/download-video/${token}`;
+    }
+    return res.json(result);
 };
