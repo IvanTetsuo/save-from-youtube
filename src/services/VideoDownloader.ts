bb6ebe653c2771b19afb83cc6f385662bb0a5146
IvanTetsuo <ivan.d2413@gmail.com>
2025-02-13 15:42:35 +0300

    изменён сервис скачивания: добавлена генерация названия, отслеживание состояния

--- src/services/VideoDownloader.ts
+++ src/services/VideoDownloader.ts
@@ -15,29 +15,46 @@ class VideoDownloader {
         return job.id.toString();
     }
 
-    async downloadVideo(url: string, job_id: string): Promise<boolean> {
-        return new Promise((resolve) => {
-            if (!ytdl.validateURL(url)){
-                resolve(false);
-            }
-            const filePath = path.join('./videos', `${job_id}_video.mp4`);
-            const fileStream = fs.createWriteStream(filePath);
-            ytdl(url).pipe(fileStream); //info
+    async downloadVideo(url: string, job_id: string): Promise<boolean | undefined> {
+        try {
+            return new Promise((resolve) => {
+                if (!ytdl.validateURL(url)){
+                    resolve(false);
+                }
+                const filePath = this.getVideoFilePath(job_id);
+                const fileStream = fs.createWriteStream(filePath);
+                const ytdlUrl = ytdl(url); //info
+                const ytdlUrlPipe = ytdlUrl.pipe(fileStream);
 
-            fileStream.on('finish', () => {
-                console.log(`Видео сохранено в: ${filePath}`);
-                resolve(true);
+                fileStream.on('finish', () => {
+                    console.log(`Видео сохранено в: ${filePath}`);
+                    resolve(true);
+                });
+                fileStream.on('error', (err: any) => {
+                    console.error('Ошибка при записи файла:', err);
+                    resolve(false);
+                });
+                ytdlUrl.on('error', (err: any) => {
+                    console.error('Ошибка при записи файла:', err);
+                    resolve(false);
+                });
             });
-            fileStream.on('error', (err) => {
-                console.error('Ошибка при записи файла:', err);
-                resolve(false);
-            });
-        });
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     async getJobStatus(token: string): Promise<"unknown" | JobState> {
         const job = await videoDownloadingQueue.getJob(token);
         return await job?.getState();
+    }
+
+    getVideoFileName(token: string): string {
+        return `${token}_video.mp4`;
+    }
+
+    getVideoFilePath(token: string): string {
+        return path.join('./videos', this.getVideoFileName(token));
     }
 }
 
